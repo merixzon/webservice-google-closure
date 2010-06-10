26db7c0aa839c0b4ed48f14576d376534a98d11e
Magnus Erixzon <magnus@erixzon.com>
2010-06-10 10:03:24 +0100

    Add JSON as prerequisite

--- lib/WebService/Google/Closure.pm
+++ lib/WebService/Google/Closure.pm
@@ -9,7 +9,7 @@ use File::Slurp qw( slurp );
 use WebService::Google::Closure::Types qw( ArrayRefOfStrings CompilationLevel );
 use WebService::Google::Closure::Response;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 $VERSION = eval $VERSION;
 
 has js_code => (
